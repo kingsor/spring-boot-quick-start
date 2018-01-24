ba7fed12ef516f8bcf9ecfc9521f7a8e96d64f49
kingsor <avventureweb@gmail.com>
2018-01-24 18:47:31 +0100

    initial commit

---
+++ src/main/java/com/neetpiq/spring/HelloController.java
@@ -0,0 +1,18 @@
+package com.neetpiq.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot :-D";
+	}
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello from Spring Boot";
+	}
+}
---
+++ src/main/java/com/neetpiq/spring/SpringBootQuickStartApp.java
@@ -0,0 +1,33 @@
+package com.neetpiq.spring;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootQuickStartApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuickStartApp.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			System.out.println(String.format("There are %d beans", beanNames.length));
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+}
